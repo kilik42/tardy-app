6a3ff2e40474186a026d69b9224beb1cf2205d42
kilik42 <marvinevins@yahoo.com>
2022-11-21 23:08:32 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+#get excel file
+from excel2json import convert_from_file
+
+workbook = convert_from_file('MOCK_DATA.xlsx')
+
+#convert to csv
+
+print(workbook)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("stuff")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
